8ae59434b6751650091f7234172842b051d9fc06
hila-abachi <igalk474@gmail.com>
2020-04-21 04:53:41 +0300

    files

---
+++ pages/about/index.tsx
@@ -0,0 +1,72 @@
+import axios from 'axios';
+// @ts-ignore
+import useSWR from 'swr';
+import styles from './index.module.scss';
+import Head from "next/head";
+import dynamic from 'next/dynamic';
+
+import {GetServerSideProps, GetStaticProps} from "next";
+
+const HelloComponent = dynamic(() => import('../../components/hello'));
+
+
+function HTTP_Get(url: string) {
+    //const fetcher = url => fetch(url).then(r => r.json())
+
+    console.log("GET: " + url);
+    return axios.get(url)
+        .then(res => {
+            console.log(res.data);
+            return res.data;
+        });
+}
+
+export default function About({posts}) {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Create Next About</title>
+                <meta name="Description" content="Create Next About"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+
+            <HelloComponent/>
+            <div>About:</div>
+            < ul>
+                {
+                    (GetPosts())
+                }
+            </ul>
+        </div>
+    );
+}
+
+
+//client side fetching on every visit , good for a specific dynamic result
+function GetPosts(): any {
+    const {data, error} = useSWR('https://jsonplaceholder.typicode.com/posts', HTTP_Get);
+
+    if (error != null) return <li>failed to load</li>
+    if (data == null) return <li>loading...</li>
+    return data.map(post => (
+        <li>{post.title}</li>
+    ))
+}
+
+
+//ssg good for large static result:
+//SSG(server side generation): precompiled json file of result for static export
+// export const getStaticProps: GetStaticProps = async context => {
+//     const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+//     const posts = await res.data;
+//
+//     return {
+//         props: {
+//             posts: posts,
+//         },
+//     }
+// };
+
+
+//SSR(server side rendering): can not be exported to static nginx , uses node.js
+// export const getServerSideProps: GetServerSideProps = async context => {
---
+++ pages/index.tsx
@@ -0,0 +1,79 @@
+import Head from 'next/head';
+import styles from './index.module.scss';
+import Link from "next/link";
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Create Next Home</title>
+                <meta name="Description" content="Create Next Home"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+
+            <div className={styles.main}>
+                <ul>
+                    <li>
+                        <Link href="/">
+                            <a>Home</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/about">
+                            <a>About Us</a>
+                        </Link>
+                    </li>
+                </ul>
+
+                <h1 className={styles.title}>
+                    Welcome to <a href="https://nextjs.org">Next.js!</a>
+                </h1>
+
+                <p className={styles.description}>
+                    Get started by editing <code>pages/index.js</code>
+                </p>
+
+                <div className={styles.grid}>
+                    <a href="https://nextjs.org/docs" className={styles.card}>
+                        <h3>Documentation &rarr;</h3>
+                        <p>Find in-depth information about Next.js features and API.</p>
+                    </a>
+
+                    <a href="https://nextjs.org/learn" className={styles.card}>
+                        <h3>Learn &rarr;</h3>
+                        <p>Learn about Next.js in an interactive course with quizzes!</p>
+                    </a>
+
+                    <a
+                        href="https://github.com/zeit/next.js/tree/master/examples"
+                        className={styles.card}
+                    >
+                        <h3>Examples &rarr;</h3>
+                        <p>Discover and deploy boilerplate example Next.js projects.</p>
+                    </a>
+
+                    <a
+                        href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                        className={styles.card}
+                    >
+                        <h3>Deploy &rarr;</h3>
+                        <p>
+                            Instantly deploy your Next.js site to a public URL with ZEIT Now.
+                        </p>
+                    </a>
+                </div>
+            </div>
+
+            <div className={styles.footer}>
+                <a
+                    href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by <img src="/zeit.svg" alt="ZEIT Logo"/>
+                </a>
+            </div>
+
+        </div>
+    )
+}
